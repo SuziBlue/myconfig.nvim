404a31f79634676dafba68bc71af562ba83aef39
Suziblue <arabellaovertime@gmail.com>
2025-04-28 19:49:32 +0900

    my latest config

--- init.lua
+++ init.lua
@@ -294,6 +294,8 @@ require('lazy').setup({
     },
   },
 
+  { import = 'custom.plugins' },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -948,61 +950,54 @@ require('lazy').setup({
     --   vim.opt.background = 'dark' -- set this to dark or light
     --   vim.cmd.colorscheme 'oxocarbon'
     -- end,
-    'catppuccin/nvim',
-    lazy = true,
-    name = 'catppuccin',
-    opts = {
-      transparent_background = true,
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        fzf = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { 'undercurl' },
-            hints = { 'undercurl' },
-            warnings = { 'undercurl' },
-            information = { 'undercurl' },
-          },
-        },
-        navic = { enabled = true, custom_bg = 'lualine' },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        snacks = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-    specs = {
-      {
-        'akinsho/bufferline.nvim',
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ''):find 'catppuccin' then
-            opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
-          end
-        end,
-      },
-    },
+    -- 'catppuccin/nvim',
+    -- name = 'catppuccin',
+    -- priority = 1000,
+    -- config = function()
+    --   require('catppuccin').setup {
+    --     transparent_background = true,
+    --     integrations = {
+    --       aerial = true,
+    --       alpha = true,
+    --       cmp = true,
+    --       dashboard = true,
+    --       flash = true,
+    --       fzf = true,
+    --       grug_far = true,
+    --       gitsigns = true,
+    --       headlines = true,
+    --       illuminate = true,
+    --       indent_blankline = { enabled = true },
+    --       leap = true,
+    --       lsp_trouble = true,
+    --       mason = true,
+    --       markdown = true,
+    --       mini = true,
+    --       native_lsp = {
+    --         enabled = true,
+    --         underlines = {
+    --           errors = { 'undercurl' },
+    --           hints = { 'undercurl' },
+    --           warnings = { 'undercurl' },
+    --           information = { 'undercurl' },
+    --         },
+    --       },
+    --       navic = { enabled = true, custom_bg = 'lualine' },
+    --       neotest = true,
+    --       neotree = true,
+    --       noice = true,
+    --       notify = true,
+    --       semantic_tokens = true,
+    --       snacks = true,
+    --       telescope = true,
+    --       treesitter = true,
+    --       treesitter_context = true,
+    --       which_key = true,
+    --     },
+    --   }
+
+    --   vim.cmd.colorscheme 'catppuccin'
+    -- end,
   },
 
   -- Highlight todo, notes, etc in comments
